0e26b21e1f04d9792ed4369cedeaf3817f700487
kumarsaurabh6039 <kumarsaurabh95912@gmail.com>
2025-03-03 11:35:44 +0530

    Created a new app called customers and added it to setting.py files installed app

--- projectAlpha/projectAlpha/settings.py
+++ projectAlpha/projectAlpha/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'customers',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'projectAlpha.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+# DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+DATABASES ={
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'projectAlphaDB',
+        'HOST': 'localhost', #127.0.0.1
+        'PORT': '1433',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+            'extra_params': 'Trusted_Connection=yes;'
+        },
     }
 }
 
